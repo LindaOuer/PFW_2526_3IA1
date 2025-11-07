cf62d4acb5a0a41c121a6214bcb78f780953c929
LindaOuer <linda.ouerfelli@gmail.com>
2025-11-07 11:26:17 +0100

    register

--- UserApp/views.py
+++ UserApp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import RegisterForm
 
-# Create your views here.
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = RegisterForm
+    success_url = reverse_lazy('login')
--- firstproject/settings.py
+++ firstproject/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'UserApp.User'
+
+LOGIN_REDIRECT_URL = 'conference_list'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL='login'
